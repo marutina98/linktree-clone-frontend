9f2e29f6aaa6ded7a8bb8adaf675569f1824fe13
marutina98 <martinamineo98@gmail.com>
2025-03-18 18:07:14 +0100

    Feat: base for tabs in dashboard made

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,14 +1,66 @@
 <script setup lang="ts">
 
+  import { ref, computed, useTemplateRef } from 'vue';
+
   import DashboardLinksEditor from './DashboardLinksEditor.vue';
   import DashboardProfileEditor from './DashboardProfileEditor.vue';
+
+  const tabs = [
+    { name: 'Profile', component: DashboardProfileEditor },
+    { name: 'Links', component: DashboardLinksEditor }
+  ]
+
+  const currentTab = ref(0);
+
+  const changeCurrentTab = (tabIndex: number) => {
+    currentTab.value = tabIndex;
+    makeCurrentTabActive(tabIndex);
+  }
+
+  const makeCurrentTabActive = (tabIndex: number) => {
+
+    const tabElements = document.querySelectorAll('.tab');
+    const tabElementsArray = Array.from(tabElements);
+
+    const activeTabClass = `tab-${tabIndex}`;
+
+    tabElementsArray.forEach((element: HTMLElement) => {
+
+      const isActiveTab = [...element.classList].includes(activeTabClass);
+
+      if (isActiveTab) {
+        element.classList.add('active');
+      } else {
+        element.classList.remove('active');
+      }
+
+    });
+
+  }
 
 </script>
 
 <template>
-  <div class="tabs">
+
+  <div class="container">
+
+    <ul class="tabs">
+      <template v-for="(tab, index) of tabs" key="index">
+        <li v-if="index === currentTab" @click="changeCurrentTab(index)" :class="['tab', `tab-${index}`, 'active']">{{ tab.name }}</li>
+        <li v-else="index === currentTab" @click="changeCurrentTab(index)" :class="['tab', `tab-${index}`]">{{ tab.name }}</li>
+      </template>
+    </ul>
+
+    <component :is="tabs[currentTab].component"></component>
 
   </div>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+
+  .active {
+    background-color: red;
+  }
+
+</style>
--- src/components/dashboard/DashboardLinksEditor.vue
+++ src/components/dashboard/DashboardLinksEditor.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Fugit officia accusantium nemo maiores aliquid aperiam quis,
-    odio quam doloribus architecto! Saepe quos cum eaque ipsa vel
-    similique, hic labore provident.
+    Links
   </p>
 </template>
 <style scoped></style>
