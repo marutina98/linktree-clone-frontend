1201ccaf84c0a31128f4ee59598f028af97f20ab
marutina98 <martinamineo98@gmail.com>
2025-03-19 15:44:48 +0100

    Feat: starting to work on dashboard modal

--- src/components/dashboard/DashboardEditProfileModal.vue
+++ src/components/dashboard/DashboardEditProfileModal.vue
@@ -22,14 +22,20 @@
 </script>
 
 <template>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-  Consectetur dolores odio a maiores minus, eaque iusto saepe
-  reiciendis voluptate sequi. Quaerat quisquam consectetur impedit.
-  Nulla, animi! Facere ab repudiandae temporibus!
+  <div class="modal">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Consectetur dolores odio a maiores minus, eaque iusto saepe
+    reiciendis voluptate sequi. Quaerat quisquam consectetur impedit.
+    Nulla, animi! Facere ab repudiandae temporibus!
+  </div>
 </template>
 
 <style scoped>
 
   @reference 'tailwindcss';
+
+  .modal {
+    background-color: red;
+  }
 
 </style>
