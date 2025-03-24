7540cdfc067240b9aad875da61101996d3049b43
marutina98 <martinamineo98@gmail.com>
2025-03-25 00:56:10 +0100

    Feat: working on dashboard

--- src/components/dashboard/DashboardLinksEditor.vue
+++ src/components/dashboard/DashboardLinksEditor.vue
@@ -157,56 +157,103 @@
   <div class="grid">
 
     <div class="editor">
+
+      <h1 class="editor-header">Editor</h1>
+
+      <div class="editor-content">
   
-      <template v-if="user && user.links.length > 0">
+        <template v-if="user!.links.length > 0">
 
-        <ul class="editor-links">
+          <ul class="editor-links">
 
-          <li class="editor-link" v-for="link of user.links">
+            <li class="editor-link" v-for="link of user!.links" :key="link.id">
 
-            <span class="editor-link-icon">
-              <template v-if="link.icon.length > 0">
-                {{ getIconChar(link.icon) }}
-              </template>
-            </span>
-            
-            <a :href="link.url" class="editor-link-icon-anchor">
-              {{ link.name }}
-            </a>
+              <span class="editor-link-icon">
+                <template v-if="link.icon.length > 0">
+                  {{ getIconChar(link.icon) }}
+                </template>
+              </span>
+              
+              <a :href="link.url" class="editor-link-anchor">
+                {{ link.name }}
+              </a>
 
-            <div class="editor-link-position">
+              <div class="editor-link-position">
 
-              <template v-if="link.order > 1">
-                <button @click="moveLinkUp(link.id)" class="position-btn move-up">
-                  <ChevronUpIcon class="position-btn-icon size-6" />
-                </button>
-              </template>
+                <template v-if="link.order > 1">
+                  <button @click="moveLinkUp(link.id)" class="position-btn move-up">
+                    <ChevronUpIcon class="position-btn-icon size-6" />
+                  </button>
+                </template>
 
-              <template v-if="link.order < getMaxOrder(user.links)">
-                <button @click="moveLinkDown(link.id)" class="position-btn move-down">
-                  <ChevronDownIcon class="position-btn-icon size-6" />
-                </button>
-              </template>
+                <template v-if="link.order < getMaxOrder(user!.links)">
+                  <button @click="moveLinkDown(link.id)" class="position-btn move-down">
+                    <ChevronDownIcon class="position-btn-icon size-6" />
+                  </button>
+                </template>
 
-            </div>
+              </div>
 
-          </li>
+            </li>
 
-        </ul>
+          </ul>
 
-      </template>
+        </template>
 
-      <template v-else>
-        <!-- @todo: no links -->
-      </template>
+        <template v-else>
+          <div class="editor-link">
+            No Links were found.
+          </div>
+        </template>
+
+      </div>
 
     </div>
 
     <div class="preview">
-      Preview
+
+      <h1 class="preview-header">Preview</h1>
+
+      <div class="preview-content">
+      
+        <div class="preview-picture">
+          <div :style="{ backgroundImage: `url(${user!.profile.picture})` }"></div>
+        </div>
+
+        <template v-if="user!.links.length > 0">
+
+          <ul class="preview-links">
+
+            <li class="preview-link" v-for="link of user!.links" :key="link.id">
+
+              <span class="preview-link-icon">
+                <template v-if="link.icon.length > 0">
+                  {{ getIconChar(link.icon) }}
+                </template>
+              </span>
+              
+              <a :href="link.url" class="preview-link-anchor">
+                {{ link.name }}
+              </a>
+
+            </li>
+
+          </ul>
+
+        </template>
+
+        <template v-else>
+          <div class="preview-link">
+            No Links were found.
+          </div>
+        </template>
+
+      </div>
+
     </div>
 
   </div>
+
 </template>
 
 <style scoped>
@@ -217,40 +264,14 @@
     @apply grid grid-cols-2;
   }
 
+  /* EDITOR */
+
   .editor {
-    @apply p-2;
-  }
-
-  .editor-links {
-    @apply flex flex-col gap-2;
-  }
-
-  .editor-link {
-    @apply flex justify-between p-2;
-  }
-
-  .editor-link:nth-child(even) {
-    @apply  bg-amber-100;
-  }
-
-  .editor-link:nth-child(even):hover {
-    @apply  bg-amber-200;
-  }
-
-  .editor-link:nth-child(odd) {
-    @apply bg-lime-100;
-  }
-
-  .editor-link:nth-child(odd):hover {
-    @apply  bg-lime-200;
+    @apply border-r border-dashed border-gray-200;
   }
 
   .editor-link-position {
     @apply flex flex-row gap-1 ml-1;
-  }
-
-  .preview {
-
   }
 
   .position-btn {
@@ -263,6 +284,60 @@
 
   .position-btn:hover .position-btn-icon {
     @apply bg-gray-200;
+  }
+
+  /* PREVIEW */
+
+  .preview-picture {
+    @apply flex items-center justify-center;
+  }
+
+  .preview-picture div {
+    @apply border-8 border-white h-42 ring-2 ring-gray-100 rounded-full w-42;
+    background-position: center;
+    background-size: cover;
+  }
+
+  /* EDITOR & PREVIEW */
+
+  .editor-header,
+  .preview-header {
+    @apply p-4 text-2xl text-center border-b border-dashed border-b-gray-200 uppercase;
+  }
+
+  .editor-content,
+  .preview-content {
+    @apply p-4 flex flex-col gap-4;
+  }
+
+  .editor-links,
+  .preview-links {
+    @apply flex flex-col gap-2;
+  }
+
+  .editor-link,
+  .preview-link {
+    @apply flex justify-between p-2;
+  }
+
+  .editor-link:nth-child(odd),
+  .preview-link:nth-child(odd) {
+    @apply  bg-amber-100;
+  }
+
+  .editor-link:nth-child(odd):hover,
+  .preview-link:nth-child(odd):hover {
+    @apply  bg-amber-200;
+  }
+
+  .editor-link:nth-child(even),
+  .preview-link:nth-child(even) {
+    @apply bg-lime-100;
+  }
+
+  .editor-link:nth-child(even):hover,
+  .preview-link:nth-child(even):hover {
+    @apply  bg-lime-200;
   }
 
 </style>
