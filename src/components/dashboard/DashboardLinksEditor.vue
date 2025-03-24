87080dba424d7d3800daf2c1c6fadc05e1411654
marutina98 <martinamineo98@gmail.com>
2025-03-24 15:30:36 +0100

    Feat: working in dashboard

--- src/components/dashboard/DashboardLinksEditor.vue
+++ src/components/dashboard/DashboardLinksEditor.vue
@@ -2,6 +2,11 @@
 
   import { computed } from 'vue';
 
+  import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/vue/24/solid';
+
+  import type { IUser } from '@/interfaces/user.interface';
+  import type { ILink } from '@/interfaces/link.interface';
+
   const props = defineProps({
     user: {
       type: [Object, null],
@@ -9,7 +14,25 @@
     }
   });
 
-  const user = computed(() => props.user);
+  const user = computed(() => props.user as IUser);
+
+  // To move into helpers.service.ts
+
+  const getIconChar = (icon: string) => {
+    return String.fromCodePoint(parseInt(icon, 16));
+  }
+
+  const getMaxOrder = (links: ILink[]) => {
+    return Math.max(...links.map(link => link.order));
+  }
+
+  const moveLinkDown = (id: number) => {
+    console.log('Move Link Down');
+  }
+
+  const moveLinkUp = (id: number) => {
+    console.log('Move Link Up');
+  }
 
 </script>
 
@@ -17,7 +40,47 @@
   <div class="grid">
 
     <div class="editor">
-      Editor
+  
+      <template v-if="user.links.length > 0">
+
+        <ul class="editor-links">
+
+          <li class="editor-link" v-for="link of user.links">
+
+            <span v-if="link.icon.length > 0" class="editor-link-icon">
+              {{ getIconChar(link.icon) }}
+            </span>
+            
+            <a :href="link.url" class="editor-link-icon-anchor">
+              {{ link.name }}
+            </a>
+
+            <div class="editor-link-position">
+
+              <template v-if="link.order > 1">
+                <button @click="moveLinkUp(link.id)" class="position-btn move-up">
+                  <ChevronUpIcon class="position-btn-icon size-6" />
+                </button>
+              </template>
+
+              <template v-if="link.order < getMaxOrder(user.links)">
+                <button @click="moveLinkDown(link.id)" class="position-btn move-down">
+                  <ChevronDownIcon class="position-btn-icon size-6" />
+                </button>
+              </template>
+
+            </div>
+
+          </li>
+
+        </ul>
+
+      </template>
+
+      <template v-else>
+        <!-- @todo: no links -->
+      </template>
+
     </div>
 
     <div class="preview">
@@ -39,8 +102,32 @@
 
   }
 
+  .editor-links {
+    @apply flex flex-col gap-2;
+  }
+
+  .editor-link {
+    @apply flex bg-amber-100 justify-between p-2;
+  }
+
+  .editor-link-position {
+    @apply flex flex-row gap-1 ml-1;
+  }
+
   .preview {
 
+  }
+
+  .position-btn {
+    @apply cursor-pointer;
+  }
+
+  .position-btn-icon {
+    @apply aspect-square bg-white font-bold p-1 rounded-full;
+  }
+
+  .position-btn:hover .position-btn-icon {
+    @apply bg-gray-200;
   }
 
 </style>
