b899b439738610d37a5b636c699e58220662bf21
marutina98 <martinamineo98@gmail.com>
2025-03-26 14:34:00 +0100

    Feat: added css

--- src/components/dashboard/DashboardAddLinkModal.vue
+++ src/components/dashboard/DashboardAddLinkModal.vue
@@ -76,10 +76,56 @@
 
 <template>
   <form ref="form" @submit.prevent="onSubmit">
-    <button ref="submit-btn" type="submit"></button>
+    <button ref="submit-btn" type="submit">Submit</button>
   </form>
 </template>
 
 <style scoped>
+
+  @reference 'tailwindcss';
+
+  form {
+    @apply bg-white flex flex-col gap-6 p-8 rounded-lg shadow-gray-100 shadow-md;
+  }
+
+  form fieldset {
+    @apply flex flex-col;
+  }
+
+  form label {
+    @apply font-bold mb-2 text-gray-600 text-sm;
+  }
+
+  form input {
+    @apply bg-gray-50 border border-gray-100 p-2 rounded-lg shadow-gray-100 shadow-md;
+  }
+
+  form input:focus {
+    @apply outline-2 outline-amber-500;
+  }
+
+  form input[aria-invalid='true'] {
+    @apply outline-2 outline-red-500;
+  }
+
+  button[type='submit'] {
+    @apply bg-amber-300 font-bold inset-shadow-sm py-3 px-4 rounded-lg shadow-gray-100 shadow-md text-sm text-white;
+  }
+
+  button[type='submit']:hover {
+    @apply bg-amber-400;
+  }
+
+  button[type='submit']:disabled {
+    @apply bg-gray-300 cursor-not-allowed;
+  }
+
+  .errors {
+    @apply list-disc;
+  }
+
+  .error {
+    @apply text-sm;
+  }
 
 </style>
